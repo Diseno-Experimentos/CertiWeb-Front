aa4ad31dc5b18a6144902b326fe60370807b45eb
Anghel Melanie Trillo Hernández <amelanie.trillo27@gmail.com>
2025-11-26 04:00:53 -0500

    Update API key for imgbb in development environment

--- src/environments/environment.development.js
+++ src/environments/environment.development.js
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     //serverBasePath: 'http://localhost:3000',
-    api_key_imgbb: '5e8520a4a581822f7aef6ae42d2e407b',
+    api_key_imgbb: 'a18cd2c512534727c84c3fe5bb213e15',
     serverBasePath: "https://certiweb-backend-ftbk.onrender.com/api/v1"
 };
